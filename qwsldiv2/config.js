5538860b7970d975a89c5eae21a0b80290d2bb20
Bogusz <bogusz.dybowski@gmail.com>
2025-12-18 19:37:11 +0000

    - working on the web app

---
+++ qwsldiv2/config.js
@@ -0,0 +1,16 @@
+/**
+ * Central configuration for QuakeWorld 4on4 Tournament SPA
+ */
+const CONFIG = {
+  appTitle: "QuakeWorld Super League DIV2",
+  defaultPage: "standings",
+  logoUrl: "https://www.quakeworld.nu/w/images/6/62/Qwsldiv2-logo.png",
+  wikiUrl: "https://www.quakeworld.nu/wiki/QWSL_DIV2",
+  apiEndpoints: {
+    standings: "standings",
+    teams: "teams",
+    players: "players",
+    games: "games"
+  },
+  
+};
